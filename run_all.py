5f1e49c5127c2b7fb4c6a0263935b550eaac1c4c
Ziming <protectione055@foxmail.com>
2023-12-31 12:23:42 +0800

    feat(postgresql_task3_impl): 实现ifc转换为postgresql中的关系表

--- run_all.py
+++ run_all.py
@@ -1,18 +1,27 @@
 import os
+import configparser
 
 from task3_cost_estmation.rawfile_task3_impl import RawfileTask3Impl
-from task3_cost_estmation.sqlite_task3_impl import SQLiteTask3Impl
+from task3_cost_estmation.postgresql_task3_impl import PGTask3Impl
 
 # 指定测试的实现类
-# test_class = "rawfile"
-test_class = "sqlite"
+test_class = "POSTGRESQL"
+
+# 读取配置文件
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 class TaskFactory:
     def get_task3(self, test_class):
-        if test_class == "rawfile":
+        if test_class == "RAWFILE":
             return RawfileTask3Impl()
-        elif test_class == "sqlite":
-            return SQLiteTask3Impl()
+        elif test_class == "POSTGRESQL":
+            user = config.get('POSTGRESQL', 'user')
+            password = config.get('POSTGRESQL', 'password')
+            host = config.get('POSTGRESQL', 'host')
+            port = config.get('POSTGRESQL', 'port')
+            args = {"user": user, "password": password, "host": host, "port": port}
+            return PGTask3Impl(args)
         else:
             raise Exception("No such test class: " + test_class)
 
@@ -21,7 +30,7 @@ if "__main__" == __name__:
     pwd = os.getcwd()
     
     # Task3
-    task3_workloads = ["datasets/task3/20210219Architecture.ifc"]
+    task3_workloads = ["datasets/task3/workload1.ifc"]
     for i in range(len(task3_workloads)):
         task3_workloads[i] = os.path.join(pwd, task3_workloads[i])
     task3 = task_facotry.get_task3(test_class)
